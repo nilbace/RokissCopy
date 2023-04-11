bc5ef160f584c089e8ac4828c99f3ef442a4fede
nilbace <nilbace@naver.com>
2023-04-11 13:45:03 +0900

    뭐한거야
    모르겠다?

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -11,4 +11,13 @@ public class TestCollision : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         Debug.Log("trig");
     }
+
+    private void Update() {
+        Debug.DrawRay(transform.position, Vector3.forward*10, Color.red);
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, Vector3.forward, out hit, 10))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
 }
